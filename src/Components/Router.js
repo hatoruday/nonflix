24852af6e7a9e16fa320a0b780f557b9d05a0bb3
hatoruday <gkgkeogns@naver.com>
2021-07-16 15:54:56 +0900

    3.4

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route, Redirect, Switch  } from "react-router-
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 const router =  () => (
     <Router>
+        <>
+        <Header />
         <Switch>
-            
             <Route path = "/" exact  component = {Home} />
             <Route path = "/tv"  component = {TV} />
             <Route path = "/search"  component = {Search} />
             <Redirect from = "*" to = "/" />
             
         </Switch>  
+        </>
     </Router>
 
 );
